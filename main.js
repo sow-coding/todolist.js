bd40bcc22e2c596c8748e8023b7478fe7f817c10
Saikou Sow <sow.coding@gmail.com>
2024-10-02 07:20:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const newTaskButton = document.getElementById("newTaskButton")
+const tasks = document.querySelector(".tasks")
+
+function addTask () {
+    newTaskButton.addEventListener("click", (e) => {
+        
+    })
+}
